f01fd270b4608c128d64fc0487d5120af43a8fc6
Tom Kopp <witzbould@live.com>
2018-01-04 11:13:07 +0100

    add findkey, instert

--- B-Tree/btree.h
+++ B-Tree/btree.h
@@ -22,27 +22,28 @@ typedef unsigned int stdelement;
  * struct for the b-tree data structure
  */
 struct node {
-  node_pointer parent;
-  short number_of_elements;
-  stdelement elements[MAXNODE];
-  node_pointer children[MAXNODE + 1];
+	node_pointer parent;
+	short number_of_elements;
+	stdelement elements[MAXNODE];
+	node_pointer children[MAXNODE + 1];
 };
 
-struct node_element{
-	node_pointer node;
-	short index;
-};
+// bullshit
+//struct node_element{
+//	node_pointer node;
+//	short index;
+//};
 
 struct compare_result {
-    int is_equal;
-    enum {DEPTH, NUMBER_OF_ELEMENTS, ELEMENT} difference;
-    int depth;
-    int *path;
+	int is_equal;
+	enum { DEPTH, NUMBER_OF_ELEMENTS, ELEMENT } difference;
+	int depth;
+	int *path;
 };
 
 typedef struct {
-    struct node *root;
-} * btree;
+	struct node *root;
+} *btree;
 
 btree create_btree();
 void destroy_btree(btree tree);
@@ -51,7 +52,6 @@ int get_btree_depth(btree tree);
 int render_node_to_svg(FILE* fd, node_pointer node, int x, int y);
 int save_btree_part(FILE* fd, node_pointer tree, int x, int y, int prev_stride);
 int save_btree(const char* path, btree tree);
-
 
 int compare(btree tree_a, btree tree_b, struct compare_result *result);
 void print_compare(btree tree_a, btree tree_b);
